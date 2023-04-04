97d6f235f57dc7cc1cbd34c9efbdad109f238340
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-04-04 21:09:09 +0300

    Create new collection view sections

--- ECommerceApp/Helpers/CompositionalLayoutSectionHelper.swift
+++ ECommerceApp/Helpers/CompositionalLayoutSectionHelper.swift
@@ -1,14 +1,19 @@
 import UIKit
 
 class CompositionalLayoutSectionHelper {
-    static func createProductCategoriesSection() -> NSCollectionLayoutSection {
+
+    static func createHotSalesProdutsSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
-            widthDimension: .estimated(60),
-            heightDimension: .fractionalHeight(1.0)
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(290)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(60), heightDimension: .absolute(40))
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .absolute(300)
+        )
+
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
         let section = NSCollectionLayoutSection(group: group)
@@ -22,7 +27,8 @@ class CompositionalLayoutSectionHelper {
         let topHeader = NSCollectionLayoutBoundarySupplementaryItem(
             layoutSize: topHeaderSize,
             elementKind: ProductCollectionReusableHeaderView.kind,
-            alignment: .top)
+            alignment: .top
+        )
 
         let sectionHeaderItemSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
@@ -32,9 +38,72 @@ class CompositionalLayoutSectionHelper {
         let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
             layoutSize: sectionHeaderItemSize,
             elementKind: CollectionSectionHeaderView.kind,
-            alignment: .topLeading)
+            alignment: .topLeading
+        )
 
         section.boundarySupplementaryItems = [topHeader, sectionHeader]
+
+        return section
+    }
+
+    static func createRecommendForYouProdutsSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(290)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .absolute(300)
+        )
+
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+
+        let sectionHeaderItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: sectionHeaderItemSize,
+            elementKind: CollectionSectionHeaderView.kind,
+            alignment: .topLeading
+        )
+
+        section.boundarySupplementaryItems = [sectionHeader]
+
+        return section
+    }
+
+    static func createProductCategoriesSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .estimated(60),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(60), heightDimension: .absolute(40))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+
+        let sectionHeaderItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: sectionHeaderItemSize,
+            elementKind: CollectionSectionHeaderView.kind,
+            alignment: .topLeading
+        )
+
+        section.boundarySupplementaryItems = [sectionHeader]
 
         return section
     }
