0b3c5d06327b98e1aaf5b5bf5afe198e806e71ec
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-04-04 21:11:02 +0300

    Edit section header

--- ECommerceApp/Views/CollectionSectionHeaderView/CollectionSectionHeader.swift
+++ ECommerceApp/Views/CollectionSectionHeaderView/CollectionSectionHeader.swift
@@ -1,19 +1,13 @@
 import UIKit
 
 class CollectionSectionHeaderView: UICollectionReusableView {
+
     static let kind = "sectionHeader"
     static let identifier = "sectionHeader"
 
-    private lazy var containerView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
     lazy var titleLabel: UILabel =  {
         let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 24)
-        label.numberOfLines = 0
+        label.font = Theme.AppFont.sectionTitle
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -29,7 +23,7 @@ class CollectionSectionHeaderView: UICollectionReusableView {
     }
 }
 
-// MARK: - Configure Model
+// MARK: - Configure
 extension CollectionSectionHeaderView {
     func configureModel(with title: String) {
         titleLabel.text = title
@@ -39,27 +33,18 @@ extension CollectionSectionHeaderView {
 // MARK: - Constraints
 extension CollectionSectionHeaderView {
     private func addUIElements() {
-        addSubview(containerView)
-        containerView.addSubview(titleLabel)
+        addSubview(titleLabel)
     }
 
     private func configureConstraints() {
 
-        let containerViewConstraints = [
-            containerView.topAnchor.constraint(equalTo: topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ]
-
         let titleLabelConstraints = [
-            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
         ]
 
-        NSLayoutConstraint.activate(containerViewConstraints)
         NSLayoutConstraint.activate(titleLabelConstraints)
     }
 }
