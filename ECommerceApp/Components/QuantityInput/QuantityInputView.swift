2164562af832b6901303ae519928f0705023624e
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-04-19 23:34:52 +0300

    Create quantity input view

---
+++ ECommerceApp/Components/QuantityInput/QuantityInputView.swift
@@ -0,0 +1,116 @@
+import UIKit
+
+protocol QuantityInputViewDelegate: AnyObject {
+    func didTapPlusButton()
+    func didTapMinusButton()
+}
+
+final class QuantityInputView: UIView {
+
+    weak var delegate: QuantityInputViewDelegate?
+
+    lazy var quantityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "1"
+        label.font = Theme.AppFont.productCardPrice
+        return label
+    }()
+
+    private lazy var plusButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        let image = UIImage(
+            systemName: "plus",
+            withConfiguration: UIImage.SymbolConfiguration(
+                pointSize: 8,
+                weight: .medium
+            )
+        )?.withTintColor(.white, renderingMode: .alwaysOriginal)
+
+        button.layer.cornerRadius = 4
+        button.setImage(image, for: .normal)
+        button.backgroundColor = Theme.Color.black
+        button.addTarget(self, action: #selector(didTapPlusButton), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var minusButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        let image = UIImage(
+            systemName: "minus",
+            withConfiguration: UIImage.SymbolConfiguration(
+                pointSize: 10,
+                weight: .medium
+            )
+        )?.withTintColor(.white, renderingMode: .alwaysOriginal)
+
+        button.layer.cornerRadius = 4
+        button.setImage(image, for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(didTapMinusButton), for: .touchUpInside)
+        return button
+    }()
+
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+
+        addUIElements()
+        configureConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("QuantityInput has not been implemented")
+    }
+}
+
+extension QuantityInputView {
+
+    @objc private func didTapPlusButton() {
+        delegate?.didTapPlusButton()
+    }
+
+    @objc private func didTapMinusButton() {
+        delegate?.didTapMinusButton()
+    }
+}
+
+// MARK: - Constraints
+extension QuantityInputView {
+
+    private func addUIElements() {
+        addSubview(plusButton)
+        addSubview(quantityLabel)
+        addSubview(minusButton)
+    }
+
+    private func configureConstraints() {
+
+        let plusButtonConstraints = [
+            plusButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            plusButton.leadingAnchor.constraint(equalTo: leadingAnchor),
+            plusButton.widthAnchor.constraint(equalToConstant: 16),
+            plusButton.heightAnchor.constraint(equalToConstant: 16)
+        ]
+
+        let quantityLabelConstraints = [
+            quantityLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            quantityLabel.leadingAnchor.constraint(equalTo: plusButton.trailingAnchor, constant: 8)
+        ]
+
+        let minusButtonConstraints = [
+            minusButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            minusButton.leadingAnchor.constraint(equalTo: quantityLabel.trailingAnchor, constant: 8),
+            minusButton.widthAnchor.constraint(equalToConstant: 16),
+            minusButton.heightAnchor.constraint(equalToConstant: 16)
+        ]
+
+        NSLayoutConstraint.activate(plusButtonConstraints)
+        NSLayoutConstraint.activate(quantityLabelConstraints)
+        NSLayoutConstraint.activate(minusButtonConstraints)
+    }
+}
