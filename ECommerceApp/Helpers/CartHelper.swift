955a022e710cdd33f57dad6200886f83b16cee8b
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-04-20 15:32:39 +0300

    Refactor and add new cart helper methods

--- ECommerceApp/Helpers/CartHelper.swift
+++ ECommerceApp/Helpers/CartHelper.swift
@@ -1,19 +1,62 @@
 final class CartHelper {
 
-    static func addProductToCart(product: CartProduct) {
-        let index = AppData.cart.firstIndex {
-            $0.id == product.id
-        }
+    static func addProductToCart(cartProduct: CartProduct) {
+        let index = getIndexProductFromCartByProductId(productId: cartProduct.productId)
 
         if let index = index {
-            AppData.cart[index].quantity += 1
-            AppData.cart[index].price += product.price
+            increaseProductQuantityInCart(cartProductIndex: index)
         } else {
-            AppData.cart.append(product)
+            let cartProduct = CartProduct(
+                productId: cartProduct.productId,
+                unitPrice: cartProduct.unitPrice,
+                totalPrice: cartProduct.totalPrice,
+                title: cartProduct.title,
+                description: cartProduct.description,
+                brand: cartProduct.brand,
+                category: cartProduct.category,
+                thumbnail: cartProduct.thumbnail,
+                quantity: 1
+            )
+
+            AppData.cart.append(cartProduct)
+        }
+    }
+
+    private static func increaseProductQuantityInCart(cartProductIndex: Int) {
+        AppData.cart[cartProductIndex].quantity += 1
+        AppData.cart[cartProductIndex].totalPrice += AppData.cart[cartProductIndex].unitPrice
+    }
+
+    static func decreaseProductQuantityInCart(cartProduct: CartProduct) {
+        let index = getIndexProductFromCartByProductId(productId: cartProduct.productId)
+
+        if let index = index {
+            if AppData.cart[index].quantity > 1 {
+                AppData.cart[index].quantity -= 1
+                AppData.cart[index].totalPrice -= cartProduct.unitPrice
+            } else {
+                removeProductFromCart(cartProduct: cartProduct)
+            }
+        }
+    }
+
+    static func removeProductFromCart(cartProduct: CartProduct) {
+        let index = getIndexProductFromCartByProductId(productId: cartProduct.productId)
+
+        if let index = index {
+            AppData.cart.remove(at: index)
         }
     }
 
     static func clearCart() {
         AppData.cart = []
+    }
+
+    private static func getIndexProductFromCartByProductId(productId: Int) -> Int? {
+        let index = AppData.cart.firstIndex {
+            $0.productId == productId
+        }
+
+        return index
     }
 }
