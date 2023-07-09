cebf69693185dcec45be66c51694cf98663eb650
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-07-09 22:40:38 +0300

    Create product detail ui tests

---
+++ ECommerceAppUITests/ProductDetailUITests.swift
@@ -0,0 +1,78 @@
+import XCTest
+
+final class ProductDetailUITests: XCTestCase {
+
+    func test_goToProductDetail() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let collectionView = app.collectionViews["home_product_collectionView"]
+
+        let productCell = collectionView.cells["product_cell_0_0"]
+        let cellExistsPredicate = NSPredicate(format: "exists == true")
+        let cellExistsExpectation = expectation(
+            for: cellExistsPredicate,
+            evaluatedWith: productCell,
+            handler: nil
+        )
+
+        let timeout: TimeInterval = 10
+        waitForExpectations(timeout: timeout) { error in
+            if let error = error {
+                XCTFail("Failed to fulfill the expectation: \(error)")
+            } else {
+                productCell.tap()
+                cellExistsExpectation.fulfill()
+
+                let elementsToCheck: [XCUIElement] = [
+                    app.staticTexts["title_label"],
+                    app.staticTexts["brand_label"],
+                    app.images["discount_imageView"],
+                    app.staticTexts["discount_label"],
+                    app.images["rating_imageView"],
+                    app.staticTexts["rating_label"],
+                    app.images["stock_imageView"],
+                    app.staticTexts["stock_label"],
+                    app.staticTexts["description_label"]
+                ]
+
+                elementsToCheck.forEach { label in
+                    XCTAssertTrue(
+                        label.exists,
+                        "The \(label.identifier) should be displayed on the product detail screen."
+                    )
+                }
+            }
+        }
+    }
+
+    func test_goBackAfterGoToProductDetail() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let collectionView = app.collectionViews["home_product_collectionView"]
+
+        let productCell = collectionView.cells["product_cell_0_0"]
+        let cellExistsPredicate = NSPredicate(format: "exists == true")
+        let cellExistsExpectation = expectation(
+            for: cellExistsPredicate,
+            evaluatedWith: productCell,
+            handler: nil
+        )
+
+        let timeout: TimeInterval = 10
+        waitForExpectations(timeout: timeout) { error in
+            if let error = error {
+                XCTFail("Failed to fulfill the expectation: \(error)")
+            } else {
+                productCell.tap()
+                cellExistsExpectation.fulfill()
+
+                let backButton = app.scrollViews.otherElements.buttons["back_button"]
+                backButton.tap()
+
+                XCTAssertTrue(collectionView.exists)
+            }
+        }
+    }
+}
